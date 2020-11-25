ee2a6be68a6fd4aa9d8ba12dbd2bbb5173b55e59
master chen <517455797@qq,com>
2020-11-25 22:40:56 +0800

    实现aside切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
    <div>
     <Topnav/>
     <div class="content">
-      <aside>
+      <aside v-if="menuVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -19,15 +19,79 @@
           </li>
         </ol>
       </aside>
-      <main>主内容</main>
+      <main></main>
     </div>
    </div>
 </template>
-<script>
+<script lang="ts">
 import Topnav from '../components/Topnav.vue'
-export default {
-  components: {
-    Topnav
+import { inject, Ref } from 'vue'
+  export default {
+    components: {
+      Topnav
+    },
+    setup() {
+      const menuVisible = inject<Ref<boolean>>('xxx')
+      return { menuVisible }
+    }
   }
-}
 </script>
+<style lang="scss" scoped>
+  $aside-index : 10;
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    >.nav {
+      flex-shrink: 0;
+    }
+    >.content {
+      flex-grow: 1;
+      padding-top: 60px;
+      padding-left: 156px;
+      @media (max-width: 500px) {
+        padding-left: 0;
+      }
+    }
+  }
+  .content {
+    display: flex;
+    >aside {
+      flex-shrink: 0;
+    }
+    >main {
+      flex-grow: 1;
+      padding: 16px;
+      background: white;
+    }
+  }
+  aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px 0;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+    height: 100%;
+    z-index: $aside-index;
+    >h2 {
+      margin-bottom: 4px;
+      padding: 0 16px;
+    }
+    >ol {
+      >li {
+        >a {
+          display: block;
+          padding: 4px 16px;
+          text-decoration: none;
+        }
+        .router-link-active {
+          background: white;
+        }
+      }
+    }
+  }
+  main {
+    overflow: auto;
+  }
+</style>
