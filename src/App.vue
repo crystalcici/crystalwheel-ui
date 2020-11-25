47f9abfe79a8f8d8ae228642362461c189ebf7cf
master chen <517455797@qq,com>
2020-11-25 15:58:08 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <wheel/>
+</template>
+
+<script>
+import wheel from './components/wheel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    wheel
+  }
+}
+</script>
