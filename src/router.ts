3b6fb02f5909f76fd41e3d52576b0c658944aec8
master chen <517455797@qq,com>
2020-11-26 20:01:47 +0800

    基本完成官网框架

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
+import DocDemo from './components/DocDemo.vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -15,6 +16,7 @@ export const router = createRouter({
       path: '/doc',
       component: Doc,
       children: [
+        {path: '', component: DocDemo},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
