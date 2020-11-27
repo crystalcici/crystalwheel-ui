ee5ebb6b37974b6f64e5dfee805d7194fc3849a9
master chen <517455797@qq,com>
2020-11-27 17:54:31 +0800

    接受value和input

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <Switch />
+    <Switch :value="y" @input="y = $event" />
   </div>
 </template>
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
+import Switch from "../lib/SwitchProp.vue";
 
 export default {
   components: { Switch },
+  setup() {
+    const y = ref(true);
+    return { y };
+  },
 };
 </script>
