8fbfc142a6f46dd31b5614b6fbf7ffad484bbf96
crystalcici <517455797@qq.com>
2020-12-03 23:17:34 +0800

    调整demo样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,20 +1,20 @@
 <template>
   <div>Button 实例</div>
   <h1>示例1</h1>
-  <div>
+  <div class="button1">
     <Button>Hola</Button>
     <Button theme="button">Hola</Button>
     <Button theme="link">Hola</Button>
     <Button theme="text">Hola</Button>
   </div>
   <h1>示例2</h1>
-  <div>
+  <div class="button2">
     <Button size="big">大大大</Button>
     <Button>普普通通</Button>
     <Button size="small">小小小</Button>
   </div>
   <h1>示例3</h1>
-  <div>
+  <div class="button3">
     <dir>
       <Button level="main">主要按钮</Button>
       <Button>普通按钮</Button>
@@ -32,13 +32,13 @@
     </dir>
   </div>
   <h1>示例4</h1>
-  <div>
+  <div class="button4">
     <Button disabled>禁用按钮</Button>
     <Button theme="link" disabled>禁用链接按钮</Button>
     <Button theme="text" disabled>禁用按钮</Button>
   </div>
   <h1>示例5</h1>
-  <div>
+  <div class="button5">
     <Button loading>加载中</Button>
     <Button>加载完毕</Button>
   </div>
@@ -52,4 +52,24 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.button1 {
+  display: flex;
+  flex-direction: row;
+}
+.button2 {
+  display: flex;
+  flex-direction: row;
+}
+.button3 {
+  display: flex;
+  flex-direction: row;
+}
+.button4 {
+  display: flex;
+  flex-direction: row;
+}
+.button5 {
+  display: flex;
+  flex-direction: row;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-   <div>
-   <Topnav/>
+  <div>
+    <Topnav />
     <div class="banner">
       <h1>crystalUI</h1>
       <h2>一个基于TS+Vue3的轮子学习</h2>
@@ -9,91 +9,95 @@
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
-   </div>
+  </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
   components: {
-    Topnav
-  }
-}
+    Topnav,
+  },
+};
 </script>
 <style lang="scss" scoped>
-  $green: #02bcb0;
-  $border-radius: 4px;
-  $color: #007974;
-  .topnavAndBanner {
-    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
-    clip-path: ellipse(80% 60% at 50% 40%);
-  }
-  .features {
-    margin: 64px auto;
-    padding: 0 16px;
-    @media (min-width: 800px) {
-      width: 800px;
-      >ul {
-        >li {
-          width: 50%;
-        }
-      }
-    }
-    @media (min-width: 1200px) {
-      width: 1200px;
-      >ul {
-        >li {
-          width: 33.3333%;
-        }
-      }
-    }
-    >ul {
-      display: flex;
-      flex-wrap: wrap;
-      >li {
-        margin: 16px 0;
-        display: grid;
-        justify-content: start;
-        align-content: space-between;
-        grid-template-areas:
-          "icon title"
-          "icon text";
-        grid-template-columns: 80px auto;
-        grid-template-rows: 1fr auto;
-        >svg {
-          grid-area: icon;
-          width: 64px;
-          height: 64px;
-        }
-        >h3 {
-          grid-area: title;
-          font-size: 28px;
-        }
-        >p {
-          grid-area: text
-        }
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+  clip-path: ellipse(80% 60% at 50% 40%);
+}
+.features {
+  margin: 64px auto;
+  padding: 0 16px;
+  @media (min-width: 800px) {
+    width: 800px;
+    > ul {
+      > li {
+        width: 50%;
       }
     }
   }
-  .banner {
-    color: $color;
-    padding: 100px 0;
+  @media (min-width: 1200px) {
+    width: 1200px;
+    > ul {
+      > li {
+        width: 33.3333%;
+      }
+    }
+  }
+  > ul {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    >.actions {
-      padding: 8px 0;
-      a {
-        margin: 0 8px;
-        background: $green;
-        color: white;
-        display: inline-block;
-        padding: 8px 24px;
-        border-radius: $border-radius;
-        &:hover {
-          text-decoration: none;
-        }
+    flex-wrap: wrap;
+    > li {
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      > p {
+        grid-area: text;
       }
     }
   }
+}
+.banner {
+  color: $color;
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: $green;
+      color: white;
+      display: inline-block;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+}
 </style>
