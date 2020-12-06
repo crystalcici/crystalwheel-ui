c8bbe00701a173742b61d47bbc39bf2169048c1b
crystalcici <517455797@qq.com>
2020-12-06 21:58:22 +0800

    dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,38 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toogle</Button>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  ></Dialog>
 </template>
 <script lang="ts">
 import Dialog from "../lib/DialogProp.vue";
+import Button from "../lib/ButtonProp.vue";
+import { ref } from "vue";
 export default {
   components: {
     Dialog,
+    Button,
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return {
+      x,
+      toggle,
+      f1,
+      f2,
+    };
   },
 };
 </script>
