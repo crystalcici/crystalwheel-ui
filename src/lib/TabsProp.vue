faa0630f78ebbeed0a90de5d53dd63169cec06f9
crystalcici <517455797@qq.com>
2020-12-07 20:16:27 +0800

    短暂提交

--- src/lib/TabsProp.vue
+++ src/lib/TabsProp.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     Tabs组件
+    <component v-for="items in defaults" :is="items" :key="items"/>
   </div>
 </template>
+<script lang="ts">
+import Tab from './TabProp.vue'
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if(tag.type !== Tab){
+        throw new Error('Tabs子标签必须是Tab')
+      }
+    })
+  }
+}
+</script>
