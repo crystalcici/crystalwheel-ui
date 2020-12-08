2fb577976d008f30cda860c3cfe369fc283ebd88
crystalcici <517455797@qq.com>
2020-12-08 21:07:23 +0800

    渲染嵌套插槽

--- src/lib/TabsProp.vue
+++ src/lib/TabsProp.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    Tabs组件
-    <component v-for="items in defaults" :is="items" :key="items"/>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav-item" v-for="t in titles" :key="t">{{t}}</div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" v-for="items in defaults" :is="items" :key="items"/>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -14,6 +18,49 @@ export default {
         throw new Error('Tabs子标签必须是Tab')
       }
     })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    return {
+      defaults,
+      titles
+    }
   }
 }
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
