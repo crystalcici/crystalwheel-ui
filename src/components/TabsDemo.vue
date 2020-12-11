ce755cb09c067a0cfdf4eecf12b41fe953e06a36
crystalcici <517455797@qq.com>
2020-12-11 17:03:26 +0800

    完成tabs

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,25 +1,23 @@
 <template>
-<Tabs v-model:selected="x">
-  <Tab title="导航1">内容1</Tab>
-  <Tab title="导航2">内容2</Tab>
-</Tabs>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 <script lang="ts">
-import Tabs from '../lib/TabsProp.vue'
-import Tab from '../lib/TabProp.vue'
-import {
-  ref
-} from 'vue'
+import Tabs from '../lib/TabsProp.vue';
+import Tab from '../lib/TabProp.vue';
+import { ref } from 'vue';
 export default {
   components: {
     Tabs,
-    Tab
+    Tab,
   },
   setup() {
-    const x = ref('导航2')
+    const x = ref('导航1');
     return {
-      x
-    }
-  }
-}
+      x,
+    };
+  },
+};
 </script>
