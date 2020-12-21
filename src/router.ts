fa7d35551f28b66743ad6594fab1f67192b12785
crystalcici <517455797@qq.com>
2020-12-21 21:01:17 +0800

    增加路由

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,34 @@
-import {createWebHashHistory, createRouter} from 'vue-router';
+import { createWebHashHistory, createRouter } from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
+import Intro from './components/Intro.vue'
 import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import DocDemo from './components/DocDemo.vue';
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/install.vue'
 
 const history = createWebHashHistory();
 export const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Home},
+    { path: '/', component: Home },
     {
       path: '/doc',
       component: Doc,
       children: [
-        {path: '', component: DocDemo},
-        {path: 'switch', component: SwitchDemo},
-        {path: 'button', component: ButtonDemo},
-        {path: 'dialog', component: DialogDemo},
-        {path: 'tabs', component: TabsDemo},
+        { path: '', component: DocDemo },
+        { path: 'intro', component: Intro },
+        { path: 'get-started', component: GetStarted },
+        { path: 'install', component: Install },
+        { path: 'switch', component: SwitchDemo },
+        { path: 'button', component: ButtonDemo },
+        { path: 'dialog', component: DialogDemo },
+        { path: 'tabs', component: TabsDemo },
       ],
     },
   ],
 });
-router.afterEach(() => {});
+router.afterEach(() => { });
